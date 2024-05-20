e5c4720a370dbf83389bae7d4d078bc10e3d9217
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-05-19 22:37:09 -0400

    second

--- contactForm/src/Components/Form.tsx
+++ contactForm/src/Components/Form.tsx
@@ -5,31 +5,31 @@ export const Form = () => {
   return (
     <div className="form">
       <h1>Contact Us</h1>
-      <div>
+      <div className="inputs">
         <div className="fields">
           <label htmlFor="name">First Name</label>
           <input type="text" id="firstName" name="firstName" required />
         </div>
-        <div>
+        <div className="fields">
           <label htmlFor="name">Last Name</label>
           <input type="text" id="LastName" name="LastName" required />
         </div>
-        <div>
+        <div className="fields">
           <label htmlFor="name">Email Address</label>
           <input type="email" id="email" name="email" required />
         </div>
-        <div>
+        <div className="radioInputs">
           <label htmlFor="name">Query Type</label>
-          <div>
-            <label htmlFor="general">General</label>
+          <div className="radioSelects">
+            <label htmlFor="general">General Enquiry</label>
             <input type="radio" id="general" name="genral" required />
           </div>
-          <div>
-            <label htmlFor="support">Support</label>
+          <div className="radioSelects">
+            <label htmlFor="support">Support Request</label>
             <input type="radio" id="support" name="support" required />
           </div>
         </div>
-        <div>
+        <div className="textSquare">
           <label htmlFor="message">Message</label>
           <textarea id="message" name="message" required />
         </div>
